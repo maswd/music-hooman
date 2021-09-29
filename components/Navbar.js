256996c2c0a8fa527b25c5e91072b6da0130c68e
maswd <alone.3303@gmail.com>
2021-09-29 15:55:09 +0330

    add auth

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,45 +1,38 @@
 import Link from "next/link";
 import Image from "next/image";
-
+import { useContext } from "react";
+import AuthContext from "../stores/authContext"
 export default function Navbar() {
+  const user = useContext(AuthContext);
+  console.log(user)
   return (
     <div className="container">
       <nav>
         <div className="icon">
-
-        
-        <ul >
-        <li>
-            <Link href="/register">
-            <a>ثبت نام </a>
-            </Link>
-          </li>
-          <span> / </span>
-          <li>
-            <Link href="/login">
-              <a>ورود </a>
-            </Link>
-          </li>
-         
-          
-          
+          <ul>
+            <li>
+              <Link href="/login">
+                <a> ورود به حساب </a>
+              </Link>
+            </li>
           </ul>
           <div className="shoping">
             <Link href="/#">
-            <a >
-              <Image src="/shopping-cart.png" width={20} height={20} /> سبد خرید{" "}
-            </a></Link>
-            
+              <a>
+                <Image src="/shopping-cart.png" width={20} height={20} /> سبد
+                خرید{" "}
+              </a>
+            </Link>
           </div>
-        
         </div>
         <a href="">
           <Image src="/user-svgrepo-com (2).svg" width={50} height={50} />
         </a>
-
-        <h1>
-          <span>Music</span> Hooman{" "}
-        </h1>
+        <Link href="/">
+          <h1 className="label">
+            <span>Music</span> Hooman{" "}
+          </h1>
+        </Link>
       </nav>
       <nav className="nav-2">
         <ul>
---
+++ stores/authContext.js
@@ -0,0 +1,20 @@
+import { createContext, useEffect, useState } from "react";
+import netlifyIdentify from "netlify-identity-widget";
+const AuthContext = createContext({
+  user: null,
+  login: () => {},
+  logOut: () => {},
+  authReady: false,
+});
+
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    netlifyIdentify.init();
+  }, []);
+  return <AuthContext.Provider value={user}>
+      {children}
+      </AuthContext.Provider>;
+};
+
+export default AuthContext;
